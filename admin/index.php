a233546dacaffbb7b55dc541bb1c01c8b4569f91
KamalChamlagain <kamalchamlagain15@gmail.com>
2022-12-09 15:21:59 +0545

    favicon updated

--- admin/index.php
+++ admin/index.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Iners Tech</title>
+    <link href="assets/images/Logo/favicon.png" rel="icon">
     <link href="assets/css/login.css" rel="stylesheet">
     <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
