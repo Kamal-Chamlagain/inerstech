ccd95a7d2be80feaac750aa2052b60c2cb19e083
KamalChamlagain <kamalchamlagain15@gmail.com>
2022-12-11 18:40:22 +0545

    sql file uploaded

---
+++ inerstech.sql
@@ -0,0 +1,277 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 11, 2022 at 01:54 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.0.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `inerstech`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `password` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `email`, `password`) VALUES
+(1, 'inerstech@gmail.com', '25f9e794323b453885f5181f1b624d0b');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `application`
+--
+
+CREATE TABLE `application` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `contact` bigint(20) NOT NULL,
+  `post` varchar(100) NOT NULL,
+  `cover` blob NOT NULL,
+  `resume` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `application`
+--
+
+INSERT INTO `application` (`id`, `name`, `email`, `contact`, `post`, `cover`, `resume`) VALUES
+(2, 'Kamal Chamlagain', 'kamalchamlagain15@gmail.com', 9825950295, 'Backend Developer', 0x636f7665722e706466, 0x4b616d616c20526573756d652e706466);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appointment`
+--
+
+CREATE TABLE `appointment` (
+  `id` int(11) NOT NULL,
+  `f_name` varchar(50) NOT NULL,
+  `l_name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `phone` bigint(20) NOT NULL,
+  `subject` varchar(50) NOT NULL,
+  `description` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `appointment`
+--
+
+INSERT INTO `appointment` (`id`, `f_name`, `l_name`, `email`, `phone`, `subject`, `description`) VALUES
+(1, 'Kamal', 'Chamlagain', 'kamalchamlagain15@gmail.com', 9825950295, 'Digital Marketting', 'I want to take the appointment to discuss about th'),
+(2, 'Kamal', 'Chamlagain', 'kamalchamlagain15@gmail.com', 9825950295, 'App Development ', 'I want to discuss');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+
+CREATE TABLE `contact` (
+  `id` int(11) NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `subject` varchar(120) NOT NULL,
+  `message` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `contact`
+--
+
+INSERT INTO `contact` (`id`, `name`, `email`, `subject`, `message`) VALUES
+(2, 'Ayush TImalsina', 'ayushtimalsina@gmail.com', 'Services', 'I like your services'),
+(3, 'Kamal Chamlagain', 'kamalchamlagain15@gmail.com', 'Website developement and management', 'I like the services of your website development and management'),
+(5, 'Basanta Karki', 'basantakarki5@gmail.com', 'Website developement and management', 'I like your developement services');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `services`
+--
+
+CREATE TABLE `services` (
+  `id` int(11) NOT NULL,
+  `service_name` varchar(70) NOT NULL,
+  `description` text NOT NULL,
+  `image` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `services`
+--
+
+INSERT INTO `services` (`id`, `service_name`, `description`, `image`) VALUES
+(1, 'Digital Marketing', 'We provide different digital marketing services according to the requirement', 0x61626f75742e706e67);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `team`
+--
+
+CREATE TABLE `team` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `designation` varchar(50) NOT NULL,
+  `facebook` varchar(200) NOT NULL,
+  `instagram` varchar(200) NOT NULL,
+  `linkedin` varchar(200) NOT NULL,
+  `image` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `team`
+--
+
+INSERT INTO `team` (`id`, `name`, `designation`, `facebook`, `instagram`, `linkedin`, `image`) VALUES
+(3, 'Ankit Kumar SIngh', 'Chief Executive Officer', 'https://www.facebook.com/singh.ankit.56', 'https://www.facebook.com/aayush.timalsina.9889', 'https://www.facebook.com/aayush.timalsina.9889', 0x616e6b69742e6a7067),
+(4, 'Sudan Bhattarai', 'BOD', '', '', '', 0x737564616e2e6a7067),
+(5, 'Shriya Karn', 'Manager', '', '', '', 0x7368726579612e6a7067),
+(6, 'Aayush Timalsina', 'Web Developer', 'https://www.facebook.com/aayush.timalsina.9889', 'https://www.facebook.com/aayush.timalsina.9889', 'https://www.facebook.com/aayush.timalsina.9889', 0x7072616469702e6a7067);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vacancy`
+--
+
+CREATE TABLE `vacancy` (
+  `id` int(11) NOT NULL,
+  `title` varchar(1000) NOT NULL,
+  `description` varchar(1000) NOT NULL,
+  `responsibilities` varchar(1000) NOT NULL,
+  `requirements` varchar(1000) NOT NULL,
+  `deadline` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `vacancy`
+--
+
+INSERT INTO `vacancy` (`id`, `title`, `description`, `responsibilities`, `requirements`, `deadline`) VALUES
+(3, 'Javascript Developer', 'We are looking for an energetic frontend developer for our organization', 'He/she should have to desgin the frontend using the latest frameworks like react,angular', 'basic knowledge html,css,javascript\r\nfamiliar with one of the latest javascript framework like angular,react', '2023-01-30'),
+(4, 'Backend Developer', 'We are looking for an energetic backend developer for our organization', 'Database designing', 'Basic knowledge of backend framework', '2022-12-06');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `application`
+--
+ALTER TABLE `application`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `appointment`
+--
+ALTER TABLE `appointment`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `contact`
+--
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `services`
+--
+ALTER TABLE `services`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `team`
+--
+ALTER TABLE `team`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `vacancy`
+--
+ALTER TABLE `vacancy`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `application`
+--
+ALTER TABLE `application`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `appointment`
+--
+ALTER TABLE `appointment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `contact`
+--
+ALTER TABLE `contact`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `services`
+--
+ALTER TABLE `services`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `team`
+--
+ALTER TABLE `team`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `vacancy`
+--
+ALTER TABLE `vacancy`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
